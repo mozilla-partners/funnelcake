5e88c00f4370d2a8548a5ff1f27656e9617e6acd
Nick Thomas <nrthomas@gmail.com>
2016-09-12 10:30:26 +1200

    Add funnelcake87 & 88 based on 85 & 86. XPI to be updated later on 88

---
+++ desktop/funnelcake88/distribution/extensions/@all-aboard-v1/data/js/values/values.js
@@ -0,0 +1,80 @@
+(function() {
+    var mainContainer = document.querySelector('main');
+    var answer = mainContainer.querySelector('#answer');
+    var button = mainContainer.querySelector('#show_answer');
+    var options = mainContainer.querySelectorAll('input[type="radio"]');
+    var secondaryContent = mainContainer.querySelector('#secondary_content');
+
+    /**
+     * Emits a cta_complete message to the add-on which will then assign
+     * the relevant token. Hides the main CTA button and shows a message
+     * to inform the user of their new token.
+     */
+    function mainCTAComplete() {
+        var prizeButton = document.querySelector('#prize');
+        var responseId;
+        var responseMsg;
+        var selectedOption;
+
+        // main cta action complete, notify add-on
+        addon.port.emit('cta_complete');
+
+        // loop through the radio buttons, highlighting the answer and disabling
+        // the incorrect options.
+        for (var i = 0, l = options.length; i < l; i++) {
+            var currentOption = options[i];
+
+            // while we are looping over the radio buttons, get the
+            // one which is checked, avoids another DOM traversal.
+            if (currentOption.checked) {
+                selectedOption = currentOption;
+            }
+
+            if (currentOption.getAttribute('value') === 'answer') {
+                // highlight the answer
+                answer.classList.add('answer');
+                currentOption.click();
+            } else {
+                currentOption.disabled = true;
+            }
+        }
+
+        // hide main cta button
+        button.classList.add('hidden');
+
+        // if the user does not select any of the options, fallback to the correct answer
+        responseId = !selectedOption ? 'response_c' : selectedOption.dataset['response'];
+        // get and show the response message based on the response data attribute
+        // of the checked radio button.
+        responseMsg = document.getElementById(responseId);
+        responseMsg.classList.remove('hidden');
+        responseMsg.setAttribute('aria-hidden', false);
+
+        // unhide "claim your prize!" for final sidebar
+        if (prizeButton) {
+            prizeButton.classList.remove('hidden');
+            prizeButton.setAttribute('aria-hidden', false);
+
+            prizeButton.addEventListener('click', function() {
+                addon.port.emit('intent', 'claimPrize');
+            });
+        }
+    }
+
+    button.addEventListener('click', function() {
+
+        mainCTAComplete();
+
+        // show secondary content if it exists
+        if (secondaryContent) {
+            var secondaryCTA = secondaryContent.querySelector('#secondary_cta');
+            secondaryContent.classList.remove('hide');
+
+            secondaryCTA.addEventListener('click', function(event) {
+                event.preventDefault();
+                addon.port.emit('intent', secondaryCTA.dataset.intent);
+            });
+        }
+    });
+
+})();
