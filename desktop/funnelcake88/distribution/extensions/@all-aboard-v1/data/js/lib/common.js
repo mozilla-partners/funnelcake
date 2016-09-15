87a4908ceb653376be0103b84dea8c6d625b3d0f
Nick Thomas <nthomas-mozilla@users.noreply.github.com>
2016-09-15 14:26:44 +1200

    Funnelcake 87 & 88 v2 (#32)

---
+++ desktop/funnelcake88/distribution/extensions/@all-aboard-v1/data/js/lib/common.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function attachNextButtonListener(nextButton) {
+    nextButton.addEventListener('click', function() {
+        // notify addon that we've clicked the next button
+        addon.port.emit('next_selected');
+    });
+
+    return nextButton;
+}
--- desktop/funnelcake88/distribution/extensions/@all-aboard-v1/data/js/utility/content1.js
+++ desktop/funnelcake88/distribution/extensions/@all-aboard-v1/data/js/utility/content1.js
@@ -1,9 +1,23 @@
 (function() {
     // retrieve button from the page
     var button = document.querySelector('button');
+    var nextButton = document.getElementById('next-button');
+    var nextToken = document.querySelector('#token2');
+    var selfPacedContent = document.getElementById('selfpaced');
+
+    // attach click listener to next button
+    nextButton = attachNextButtonListener(nextButton);
 
     // listen for a click on the search button
     button.addEventListener('click', function() {
+        // if this is the furtherst sidebar the user has achieved (the next token hasn't been awarded)
+        if (!nextToken.classList.contains('active')) {
+            // unhide our next button,
+            selfPacedContent.classList.remove('hidden');
+            // update aria state
+            selfPacedContent.setAttribute('aria-hidden', false);
+        }
+
         // notify addon that we've clicked the button
         addon.port.emit('intent', 'search');
         // main cta action complete, notify add-on
